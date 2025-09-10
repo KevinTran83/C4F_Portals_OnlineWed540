2d66a0b08ea59ff30743bd2201917db6d4733507
Kevin S. Tran <tanksense@gmail.com>
2025-09-10 18:42:24 +1000

    Week 3

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    public Portal partner;
+
     public void Spawn(Vector3 spawnPos, Vector3 forward)
     {
         transform.position = spawnPos;
         transform.forward  = forward;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.transform.tag == "Teleportable")
+        {
+            other.transform.position = partner.transform.position + other.transform.forward * 2;
+            other.transform.rotation = partner.transform.rotation;
+        }
     }
 }
