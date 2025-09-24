254a5f1607f2fc3c998549352caa8b54181710c0
Kevin S. Tran <tanksense@gmail.com>
2025-09-24 18:39:11 +1000

    Final Week

--- Assets/PortalSpawner.cs
+++ Assets/PortalSpawner.cs
@@ -6,11 +6,18 @@ public class PortalSpawner : MonoBehaviour
 {
     public Portal portalA, portalB;
     public LayerMask portalSurface;
+    public ParticleSystem particles;
+
+    private ParticleSystem.MainModule particlesMain;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        particlesMain = particles.main;
+
+
+        portalA.gameObject.SetActive(false);
+        portalB.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,20 +25,26 @@ public class PortalSpawner : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            particlesMain.startColor = new Color(0, 0, 1);
+            particles.Play();
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 if ( ( (1 << hit.collider.gameObject.layer) & portalSurface ) != 0)
                 portalA.Spawn(hit.point, hit.normal);
+                portalA.gameObject.SetActive(true);
             }
         }
         if (Input.GetMouseButtonDown(1))
         {
+            particlesMain.startColor = new Color(1, 0.5f, 0);
+            particles.Play();
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 if ( ( (1 << hit.collider.gameObject.layer) & portalSurface ) != 0)
                 portalB.Spawn(hit.point, hit.normal);
+                portalB.gameObject.SetActive(true);
             }
         }
     }
